aff302fc0b769aac602bed3fe0cd2ec795dc34a1
Ahmad Yusri <yusriahmad2@gmail.com>
2024-09-17 13:29:18 +0700

    #fix: sitemap error short_open_tag

--- resources/views/main-web/home/sitemap.blade.php
+++ resources/views/main-web/home/sitemap.blade.php
@@ -1,9 +1,10 @@
+{{-- WARN: Don't run autoformatter  --}}
 @php
     header('Content-Type: application/xml; charset=utf-8');
+
+    echo '<?xml version="1.0" encoding="UTF-8"?>';
+    echo '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd">';
 @endphp
-<?xml version="1.0" encoding="UTF-8"?>
-<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
-    xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd">
     @foreach ($urls as $item)
         <url>
             <loc>{{ $item['loc'] }}</loc>
@@ -12,4 +13,7 @@
             <priority>{{ $item['priority'] }}</priority>
         </url>
     @endforeach
-</urlset>
+@php
+    echo '</urlset>';
+@endphp
+
